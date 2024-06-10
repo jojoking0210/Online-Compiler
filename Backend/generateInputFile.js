51d9c256fe9819667d84c9e688251e6777766f70
Pranav Sarate <jojoking0210@gmail.com>
2024-06-10 10:14:59 +0530

    added input testcases

--- Backend/generateInputFile.js
+++ Backend/generateInputFile.js
@@ -2,20 +2,20 @@ const fs = require('fs');
 const path = require('path');
 const { v4: uuid } = require('uuid');
 
-const dirCodes = path.join(__dirname, 'codes');
+const dirInputs = path.join(__dirname, 'inputs');
 
-if (!fs.existsSync(dirCodes)) {
-    fs.mkdirSync(dirCodes, { recursive: true });
+if (!fs.existsSync(dirInputs)) {
+    fs.mkdirSync(dirInputs, { recursive: true });
 }
 
-const generateFile = async (format, content) => {
+const generateInputFile = async (input) => {
     const jobID = uuid();
-    const filename = `${jobID}.${format}`;
-    const filePath = path.join(dirCodes, filename);
-    await fs.writeFileSync(filePath, content);
-    return filePath;
+    const input_filename = `${jobID}.txt`;
+    const input_filePath = path.join(dirInputs, input_filename);
+     fs.writeFileSync(input_filePath, input);
+    return input_filePath;
 };
 
 module.exports = {
-    generateFile,
+    generateInputFile,
 };
